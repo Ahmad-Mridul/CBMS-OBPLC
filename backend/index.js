18ed4a6c923eca11548783fd4a3cdcff36178c24
Ahmad-Mridul <mridulahmed262@gmail.com>
2026-07-02 02:35:47 -0400

    added frontend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Server is running.");
+});
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port:${PORT}`);
+});
---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div >
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, error laboriosam, harum eaque dicta eligendi necessitatibus sed impedit, sunt iste optio! Debitis labore, architecto porro culpa numquam blanditiis, suscipit, alias cum magnam veniam laboriosam corporis molestiae beatae dolor impedit perferendis deserunt eum! Asperiores aspernatur consequatur quaerat beatae? Eaque hic dolores exercitationem animi assumenda culpa consequuntur eligendi provident unde recusandae soluta mollitia id, quidem laborum sapiente possimus porro excepturi? Cum voluptatem laudantium dolores nulla labore vitae dolorem explicabo! Perferendis ratione obcaecati quo laboriosam, libero beatae? Libero officiis alias sapiente, odio itaque quasi. Assumenda pariatur a explicabo ratione molestiae ea quisquam ex.</p>
+    </div>
+  );
+}
